6a28e5e10b64736eca80b4f4ae9fa5d98b5b2066
seantokuzo <seantokuzo@gmail.com>
2022-10-13 14:55:01 -0700

    add context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { AppContextProvider } from './context/appContext'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <AppContextProvider>
+    <App />
+  </AppContextProvider>
+)
