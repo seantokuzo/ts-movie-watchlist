a9af228a5f4a32383ec8bb76cb195ebcf45ccb35
seantokuzo <seantokuzo@gmail.com>
2022-10-13 14:55:28 -0700

    import header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
-  return <div className="app"></div>
+  return (
+    <div className="app w-screen h-full">
+      <Header />
+    </div>
+  )
 }
 
 export default App
