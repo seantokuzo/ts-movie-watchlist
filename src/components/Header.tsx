f97098491f0c27c264cdca687c0d09504c85daa9
seantokuzo <seantokuzo@gmail.com>
2022-10-18 14:19:30 -0700

    adjust bg color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header: React.FC = () => {
   return (
-    <header className="w-screen bg-blue-400 py-8">
+    <header className="w-screen bg-blue-500 py-8">
       <h1 className="text-6xl font-bold text-center text-gray-100">
         Movie Mania
       </h1>
