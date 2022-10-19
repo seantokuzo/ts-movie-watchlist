ab6609cd98b2532d543b470e2fe7cc6ff42c653e
seantokuzo <seantokuzo@gmail.com>
2022-10-19 16:52:30 -0700

    add movie details component and mode

--- src/components/SharedLayout.tsx
+++ src/components/SharedLayout.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useAppContext } from '../context/appContext'
 import CurrentMovies from './CurrentMovies'
+import MovieDetails from './MovieDetails'
 
 const SharedLayout = () => {
   const { mode } = useAppContext()
-  console.log(mode)
 
   return (
     <div className="w-screen h-screen flex flex-col content-center items-center">
       {mode === 'home' && <CurrentMovies />}
+      {mode === 'details' && <MovieDetails />}
     </div>
   )
 }
