f7841a8599574e99da0d08ae4f7338dd73d6eedb
seantokuzo <seantokuzo@gmail.com>
2022-10-31 14:30:55 -0700

    conditional styling - header btns based on mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,41 @@ import React from 'react'
 import { useAppContext } from '../context/appContext'
 
 const Header: React.FC = () => {
-  const { getNowPlaying, setSearchMode } = useAppContext()
+  const { mode, getNowPlaying, setSearchMode, setWatchlistMode } =
+    useAppContext()
   return (
-    <header className="w-screen bg-blue-500 py-8 px-3">
+    <header className="w-screen bg-blue-500 py-4 px-3">
       <h1 className="text-6xl font-bold text-center text-gray-100">
         Movie Mania
       </h1>
       <div className="w-full py-2 px-3 flex flex-row flex-wrap content-center items-start">
         <div
-          className="mx-1 px-4 py-1.5 border-blue-300 border-2 rounded-l-full rounded-r-full"
+          className={`mx-1 px-4 py-1.5 border-blue-300 border-2 rounded-l-full rounded-r-full ${
+            mode === 'home' ? 'bg-blue-300' : 'bg-none'
+          }`}
           onClick={getNowPlaying}
         >
           <p className="text-lg">Now Playing</p>
         </div>
         <div
-          className="mx-1 px-4 py-1.5 border-blue-300 border-2 rounded-l-full rounded-r-full"
+          className={`mx-1 px-4 py-1.5 border-blue-300 border-2 rounded-l-full rounded-r-full ${
+            mode === 'search' ? 'bg-blue-300' : 'bg-none'
+          }`}
           onClick={setSearchMode}
         >
           <p className="text-lg flex flex-row content-center items-center">
             Search
             <i className="fa-solid fa-magnifying-glass ml-2 text-sm"></i>
+          </p>
+        </div>
+        <div
+          className={`mx-1 px-4 py-1.5 border-blue-300 border-2 rounded-l-full rounded-r-full ${
+            mode === 'watchlist' ? 'bg-blue-300' : 'bg-none'
+          }`}
+          onClick={setWatchlistMode}
+        >
+          <p className="text-lg flex flex-row content-center items-center">
+            My Watchlist
           </p>
         </div>
       </div>
