00994a0560b98ed7a7302a895488380930ec90fd
seantokuzo <seantokuzo@gmail.com>
2022-10-31 20:16:26 -0700

    build out watchlist component

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useAppContext } from '../context/appContext'
+import MovieCardBasic from './MovieCardBasic'
 
 const Watchlist: React.FC = () => {
-  return <div>Watchlist</div>
+  const { watchlist } = useAppContext()
+
+  const movieEls = (
+    <>
+      {watchlist.map((movie) => (
+        <MovieCardBasic movie={movie} key={movie.id} />
+      ))}
+    </>
+  )
+
+  return (
+    <div className="w-full max-w-4xl flex flex-col content-center items-center p-5 bg-blue-400">
+      <h2 className="text-4xl">My Watchlist</h2>
+      <div className="w-full flex flex-row flex-wrap justify-evenly items-start">
+        {movieEls}
+      </div>
+    </div>
+  )
 }
 
 export default Watchlist
