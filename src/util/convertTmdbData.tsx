039065bd9660f9d79c767eeb74fbc42b05fff396
seantokuzo <seantokuzo@gmail.com>
2022-10-31 20:09:18 -0700

    export tmdb data conversion logic to utils

---
+++ src/util/convertTmdbData.tsx
@@ -0,0 +1,40 @@
+interface TmdbMovieData {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export interface Movie {
+  id: number
+  poster: string | ''
+  title: string
+  rating: number
+  date: string
+  genre: number[]
+  plot: string
+}
+
+export const convertTmdbData = (movieData: TmdbMovieData[]): Movie[] => {
+  return movieData.map((movie) => ({
+    id: movie.id,
+    poster: movie.poster_path
+      ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+      : '',
+    title: movie.title,
+    rating: movie.vote_average,
+    date: movie.release_date ? movie.release_date.slice(0, 4) : 'N/A',
+    genre: movie.genre_ids,
+    plot: movie.overview
+  }))
+}
