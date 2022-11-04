d584aa9af9bff0bc4325496eaccdab2d6e4aba79
seantokuzo <seantokuzo@gmail.com>
2022-11-03 21:06:07 -0700

    placeholder for when 0 search results

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -32,18 +32,24 @@ const Search: React.FC = () => {
     }
   }
 
-  const movieEls = (
-    <>
-      {searchResults.map((movie) => (
-        <MovieCardBasic movie={movie} key={movie.id} />
-      ))}
-    </>
-  )
+  const movieEls =
+    searchResults.length > 0 ? (
+      <>
+        {searchResults.map((movie) => (
+          <MovieCardBasic movie={movie} key={movie.id} />
+        ))}
+      </>
+    ) : (
+      <div className="w-full mt-4 flex flex-col justify-center items-center">
+        <h3 className="text-4xl font-semibold">Find some Movies</h3>
+        <i className="fa-solid fa-film mt-2 text-8xl text-black/[0.5]"></i>
+      </div>
+    )
 
   return (
     <div className="w-full px-7 flex flex-col items-center">
       <input
-        className="w-3/4 h-[2.5rem] mt-[-1.25rem] mb-4 px-1 py-1 rounded text-lg font-medium text-center shadow-lg"
+        className="w-3/4 h-[2.5rem] mt-[-1.25rem] mb-4 px-1 py-1 rounded text-black text-lg font-medium text-center shadow-lg"
         onChange={(e) => handleSearch(e)}
         type="text"
         placeholder="Search for a movie"
@@ -51,7 +57,7 @@ const Search: React.FC = () => {
         maxLength={80}
         minLength={1}
       ></input>
-      <div className="w-full flex flex-row flex-wrap content-between items-start">
+      <div className="w-full flex flex-row flex-wrap justify-center items-start">
         {movieEls}
       </div>
     </div>
