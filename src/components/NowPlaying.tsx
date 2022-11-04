3a158859c061f033186b2ef351d70e6f3bb8fb0a
seantokuzo <seantokuzo@gmail.com>
2022-11-03 21:04:54 -0700

    update useEffect to getNoewPlaying method

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,20 +1,12 @@
 import React, { useEffect } from 'react'
 import { useAppContext } from '../context/appContext'
-// SIGN UP @ https://developers.themoviedb.org/3/getting-started/introduction to get your own API key
-import { TMDB_KEY } from '../fake.env'
 import MovieCardBasic from './MovieCardBasic'
-import { convertTmdbData } from '../util/convertTmdbData'
-
-const fetchString = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_KEY}&now-playing&popularity.gte=3000?language=en-US`
 
 const NowPlaying: React.FC = () => {
-  const { darkMode, movies, setMovies } = useAppContext()
+  const { darkMode, movies, getNowPlaying } = useAppContext()
 
   useEffect(() => {
-    fetch(fetchString)
-      .then((res) => res.json())
-      .then((data) => setMovies(convertTmdbData(data.results)))
-      .catch((err) => console.log(err))
+    getNowPlaying()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
