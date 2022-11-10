ed1b576b6fc0aeffbd3b06128d2f62da0141fa8b
seantokuzo <seantokuzo@gmail.com>
2022-11-10 13:17:46 -0800

    more responsive poster sizing

--- src/components/MovieCardBasic.tsx
+++ src/components/MovieCardBasic.tsx
@@ -7,10 +7,14 @@ const MovieCardBasic: React.FC<{ movie: Movie }> = ({ movie }) => {
   const { watchlist, getMovieDetails } = useAppContext()
 
   const moviePosterEl = movie.poster ? (
-    <img src={movie.poster} alt="Movie Poster" className="w-full"></img>
+    <img
+      src={movie.poster}
+      alt="Movie Poster"
+      className="w-full"
+    ></img>
   ) : (
     // <div className="w-full h-[14.25rem] flex flex-col justify-center items-center border-2 bg-black/[.5]">
-    <div className="w-full min-h-[12rem] flex flex-col justify-center items-center border-2 bg-black/[.5]">
+    <div className="w-full h-full min-h-[12rem] flex flex-col justify-center items-center border-2 bg-black/[.5]">
       <p
         className="text-3xl text-center text-white-400"
         style={{ textShadow: '5px 5px 5px rgba(0, 0, 0, 0.7)' }}
@@ -29,7 +33,7 @@ const MovieCardBasic: React.FC<{ movie: Movie }> = ({ movie }) => {
   }
 
   return (
-    <div className="relative w-1/2 lg:w-1/3 max-w-lg px-1 py-2 flex flex-col justify-center items-center">
+    <div className="relative w-1/2 lg:w-1/3 max-w-xs px-1 py-2 flex flex-col justify-center items-center">
       <div className="w-full" onClick={() => getMovieDetails(movie)}>
         {moviePosterEl}
         <h3 className="text-lg text-center leading-none mt-2">{movie.title}</h3>
