9c173d41f7b640297f044234fa846d0db8915493
seantokuzo <seantokuzo@gmail.com>
2022-11-18 16:08:53 -0800

    failed tempload

--- src/components/SharedLayout.tsx
+++ src/components/SharedLayout.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { useAppContext } from '../context/appContext'
 import NowPlaying from './NowPlaying'
 import MovieDetails from './MovieDetails'
-import Reviews from './Reviews'
 import Search from './Search'
 import Watchlist from './Watchlist'
 import Loading from './Loading'
 
 const SharedLayout = () => {
-  const { mode, isLoading } = useAppContext()
+  const { mode, isLoading, tempLoad } = useAppContext()
+
+  // useEffect(() => {
+  //   tempLoad()
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
   return (
     <div className="w-full h-full max-w-3xl flex flex-col justify-center items-center">
-      {mode === 'home' && <NowPlaying />}
+      {mode === 'now-playing' && <NowPlaying />}
       {mode === 'details' && <MovieDetails />}
       {mode === 'search' && <Search />}
       {mode === 'watchlist' && <Watchlist />}
