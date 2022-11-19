8d8d503d54f3f9c8f7a309e99f8cb11c044be6c4
seantokuzo <seantokuzo@gmail.com>
2022-11-18 16:04:08 -0800

    reuseable nav btns

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,10 @@
 import React from 'react'
 import { useAppContext } from '../context/appContext'
+import HeaderNavBtn from './HeaderNavBtn'
 import ThemeToggler from './ThemeToggler'
 
 const Header: React.FC = () => {
-  const {
-    darkMode,
-    mode,
-    changeModeNowPlaying,
-    setSearchMode,
-    setWatchlistMode
-  } = useAppContext()
-
-  const headerBtnColor = darkMode ? 'border-zinc-300' : 'border-blue-300'
-
-  const headerBtnClasses = `mx-1 px-4 py-1.5 ${headerBtnColor} border-2 rounded-l-full rounded-r-full hover:scale-105`
-
-  const selectedClass = (modeBtn: string) => {
-    if (mode === modeBtn) {
-      if (darkMode) return 'bg-zinc-300 text-black'
-      return 'bg-blue-300'
-    }
-    return 'bg-none'
-  }
+  const { darkMode, mode } = useAppContext()
 
   const bgColor = darkMode ? 'bg-zinc-800' : 'bg-blue-500'
 
@@ -35,31 +18,11 @@ const Header: React.FC = () => {
       <h1 className="mt-2 text-6xl font-bold text-center text-gray-100">
         Movie Mania
       </h1>
-      <div className="w-full max-w-2xl py-2 px-3 flex flex-row justify-evenly items-start">
-        <button
-          className={`${headerBtnClasses} ${selectedClass('home')}`}
-          onClick={changeModeNowPlaying}
-        >
-          <p className="text-lg md:text-lg lg:text-xl">Now Playing</p>
-        </button>
-        <button
-          className={`${headerBtnClasses} ${selectedClass('search')}`}
-          onClick={setSearchMode}
-        >
-          <p className="text-lg md:text-lg lg:text-xl flex flex-row content-center items-center">
-            Search
-            <i className="fa-solid fa-magnifying-glass ml-2 text-sm"></i>
-          </p>
-        </button>
-        <button
-          className={`${headerBtnClasses} ${selectedClass('watchlist')}`}
-          onClick={setWatchlistMode}
-        >
-          <p className="text-lg md:text-lg lg:text-xl flex flex-row content-center items-center">
-            My Watchlist
-          </p>
-        </button>
-      </div>
+      <nav className="w-full max-w-2xl py-2 px-3 flex flex-row justify-evenly items-start">
+        <HeaderNavBtn btnType="now-playing" />
+        <HeaderNavBtn btnType="search" />
+        <HeaderNavBtn btnType="watchlist" />
+      </nav>
     </header>
   )
 }
---
+++ src/components/HeaderNavBtn.tsx
@@ -0,0 +1,47 @@
+import { useAppContext } from '../context/appContext'
+import { AppMode } from '../context/appContext'
+
+const HeaderNavBtn: React.FC<{ btnType: AppMode }> = ({ btnType }) => {
+  const { mode, darkMode, setModeNowPlaying, setModeSearch, setModeWatchlist } =
+    useAppContext()
+
+  const isBtnSelected = (modeBtn: AppMode) => {
+    if (mode === modeBtn) {
+      if (darkMode) return 'bg-zinc-300 text-black'
+      return 'bg-blue-300'
+    }
+    return 'bg-none'
+  }
+
+  const handleClick = () => {
+    if (btnType === 'now-playing') {
+      return setModeNowPlaying()
+    }
+    if (btnType === 'search') {
+      return setModeSearch()
+    }
+    if (btnType === 'watchlist') {
+      return setModeWatchlist()
+    }
+  }
+
+  const btnTypeToText = btnType
+    .split('-')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
+
+  return (
+    <button
+      className={`mx-1 lg:my-2 border-2 rounded-l-full rounded-r-full hover:scale-105
+        ${darkMode ? 'border-zinc-300' : 'border-blue-300'}
+        px-4 py-1.5 sm:px-5 sm:py-2 md:px-6 md:py-3
+        text-lg sm:text-xl md:text-2xl lg:text-3xl
+        ${isBtnSelected(btnType)}`}
+      onClick={handleClick}
+    >
+      {btnTypeToText}
+    </button>
+  )
+}
+
+export default HeaderNavBtn
